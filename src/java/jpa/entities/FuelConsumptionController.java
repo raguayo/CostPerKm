33c99af8a368626536bd55cf2e6b7fe7a5af83be
Rick Aguayo <rick.aguayo@gmail.com>
2018-12-19 01:08:39 -0600

    next iteration

--- src/java/jpa/entities/FuelConsumptionController.java
+++ src/java/jpa/entities/FuelConsumptionController.java
@@ -1,16 +1,5 @@
 package jpa.entities;
 
-import jpa.entities.util.JsfUtil;
-import jpa.entities.util.PaginationHelper;
-
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.ResourceBundle;
-import javax.ejb.EJB;
-import javax.inject.Named;
-import javax.enterprise.context.SessionScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.convert.Converter;
@@ -18,6 +7,38 @@ import javax.faces.convert.FacesConverter;
 import javax.faces.model.DataModel;
 import javax.faces.model.ListDataModel;
 import javax.faces.model.SelectItem;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.persistence.TypedQuery;
+
+import javax.ejb.EJB;
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Base64;
+import java.util.ResourceBundle;
+
+import java.io.Serializable;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import jpa.entities.util.JsfUtil;
+import jpa.entities.util.PaginationHelper;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 
 @Named("fuelConsumptionController")
 @SessionScoped
@@ -29,6 +50,12 @@ public class FuelConsumptionController implements Serializable {
     private jpa.entities.FuelConsumptionFacade ejbFacade;
     private PaginationHelper pagination;
     private int selectedItemIndex;
+    
+    //TODO: setup an assignable vars to enter in 
+    //denominators for flowmeters calculation
+    private final double FLOWMETER1_DENOMINATOR = 361.0;
+    private final double FLOWMETER2_DENOMINATOR = 409.0;
+    private final double NOZZLE_DENOMINATOR = 0.0003;
 
     public FuelConsumptionController() {
     }
@@ -82,9 +109,10 @@ public class FuelConsumptionController implements Serializable {
 
     public String create() {
         try {
-            System.out.println("current: " + current);
+//            System.out.println("current: " + current);
             String strDeviceId = current.getDeviceId().toString();
-            String strFuelEntryDate = String.valueOf(current.getFuelEntryDt());
+            SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/YYYY hh:mm:ss");
+            String strFuelEntryDate = String.valueOf(sdf.format(current.getFuelEntryDt()));
             List lstSpaceSeparated = Arrays.asList(strFuelEntryDate.split(" "));
             String strDate = lstSpaceSeparated.get(0).toString();
             String strTime = lstSpaceSeparated.get(1).toString();
@@ -96,106 +124,141 @@ public class FuelConsumptionController implements Serializable {
             List lstColonSeparated = Arrays.asList(strTime.split(":"));
             String strHour = lstColonSeparated.get(0).toString();
             String strMinutes = lstColonSeparated.get(1).toString();
-            List lstPeriodSeparated = Arrays.asList(String.valueOf(lstColonSeparated.get(2)).split("."));
-            String strSeconds = lstPeriodSeparated.get(0).toString();
-            String strApiFormattedTimestamp = strYear + "-" + strMonth + "-" +
+            String strSeconds = lstColonSeparated.get(2).toString();
+            String strApiCurrFormattedTimestamp = strYear + "-" + strMonth + "-" +
                     strDay + "T" + strHour + ":" + strMinutes + ":" +
-                    strSeconds +"Z";
-            //TODO: figure out where to get previous fuel entry date (db or api) - hard coding for now                  
+                    strSeconds +"Z";               
+            
             // build out url with query parameters
+            //TODO: query db to get last row with fuel entry date and last mileageKm
+            Object[] objPrevFuelAndMileage = retrieveLastFuelEntryDateAndMileage();
+            double dblPrevMileageKm;
+            String strPrevFuelEntryDate;
+            String strApiPrevFormattedTimestamp;
+            if(objPrevFuelAndMileage == null) {
+                dblPrevMileageKm = current.getLimeageKm();
+                strApiPrevFormattedTimestamp = strApiCurrFormattedTimestamp;
+            } else {
+                dblPrevMileageKm = Double.parseDouble(String.valueOf(objPrevFuelAndMileage[1]));
+                strPrevFuelEntryDate = String.valueOf(sdf.format(objPrevFuelAndMileage[0]));
+                List lstSpaceSeparated2 = Arrays.asList(strPrevFuelEntryDate.split(" "));
+                String strDate2 = lstSpaceSeparated2.get(0).toString();
+                String strTime2 = lstSpaceSeparated2.get(1).toString();    
+                List lstSlashSeparated2 = Arrays.asList(strDate2.split("/"));
+                String strMonth2 = lstSlashSeparated2.get(0).toString();
+                String strDay2 = lstSlashSeparated2.get(1).toString();
+                String strYear2 = lstSlashSeparated2.get(2).toString();
+                List lstColonSeparated2 = Arrays.asList(strTime2.split(":"));
+                String strHour2 = lstColonSeparated2.get(0).toString();
+                String strMinutes2 = lstColonSeparated2.get(1).toString();
+                String strSeconds2 = lstColonSeparated2.get(2).toString();
+                strApiPrevFormattedTimestamp = strYear2 + "-" + strMonth2 + "-" +
+                        strDay2 + "T" + strHour2 + ":" + strMinutes2 + ":" +
+                        strSeconds2 +"Z"; 
+            }
+         
             String strBaseUrl = "http://gps.nextop.vip/api/positions";
             String strUrlWithQueries = strBaseUrl + "?deviceId=" + strDeviceId
-                    + "&amp;from=" + "2018-12-8T18:30:00Z" + "&amp;to=" + 
-                    strApiFormattedTimestamp;
-                    System.out.println("strUrlWithQueries: " + strUrlWithQueries);
-//                    
-//                    $.ajax({
-//                        type: "GET",
-//                        url: strUrlWithQueries,
-//                        success: function(jsonData) {
-////                            console.log("jsonData = " + jsonData);
-//
-//                            let dblGasRefillGals = Double.parseDoube(
-//                                    document.getElementById(
-//                                    'frmFuelEntry:gasRefillGals').value);
-//                            let dblUnitCost = Double.parseDouble(
-//                                    document.getElementById(
-//                                    'frmFuelEntry:unitCost').value);
-//                            let dblTotalCost = dblGasRefillGals * dblUnitCost;
-//                            document.getElementById('frmFuelEntry:totalCost')
-//                                    .innerHTML(String.valueOf(dblTotalCost));
-//
-//                            let objLastItem = jsonData.pop();
-//                            let strFuelVal1;
-//                            let strFuelVal2;
-//                            let strPrevFillupMileage; // TODO: need to query db or other api for this value
-//                            
-//                            // get attributes item and parse out io136 and io137
-//                            for(let property in objLastItem) {
-//                                if(objLastItem.hasOwnProperty(property)) {
-//                                    if(property === "attributes") {
-//                                        let objAttributes = objLastItem[property];
-//                                        for(let attribute in objAttributes) {
-//                                            if(attribute === "io136") {
-//                                                strFuelVal1 = property[attribute];
-//                                            }
-//                                            if(attribute === "io137") {
-//                                                strFuelVal2 = property[attribute];
-//                                            }
-//                                        }
-//                                    }
-//                                }
-//                            }
-//                            //TODO: setup an assignable vars to enter in 
-//                            //denominators for flowmeters calculation
-//                            const FLOWMETER1_DENOMINATOR = 361.0;
-//                            const FLOWMETER2_DENOMINATOR = 409.0;
-//                            
-//                            //calculate expected fuel consumption
-//                            //TODO: think about how to call previous row from db for previous fillup mileage or if value is another api call
-//                            // /api/reports/trips?deviceId=10&amp;from=2018-10-22T18:30:00Z&amp;to=2018-12-11T18:30:00Z
-//                            // formula should be (currFillupKm - prevFillupKm) / 100
-//                            let dblCurrentMileageKm = Double.parseDouble(document.getElementById('frmFuelEntryId:limeageKm').value);
-//                            let dblExpectedFuelConsumption = dblCurrentMileageKm / 100.0;                    
-//                            document.getElementById('frmFuelEntry:expectedFuelConsumption').innerHTML = String.valueOf(dblExpectedFuelConsumption);
-//                            
-//                            // calculate real fuel consumption by either nozzle 
-//                            // crocodile or flowmeters
-//                            //TODO: add if else branch for nozzle and flowmeters
-//                            
-//                            // (io136/kfactor)-(io137/kfactor)
-//                            let dblRealFuelConsumption = 
-//                                    (Double.parseDouble(strFuelVal1) / 
-//                                    FLOWMETER1_DENOMINATOR) - (Doube.parsedouble(strFuelVal2));
-//                            document.getElementById('frmFuelEntry:realConsumption').innerHTML = 
-//                                    String.valueOf(realFuelConsumption);
-//                            
-//                            // calculate difference in gallons
-//                            let dblDiffGallons = dblExpectedFuelConsumption - dblRealFuelConsumption;
-//                            document.getElementById('frmFuelEntry:diffGals').innerHTML = String.valueOf(dblDiffGallons);
-//                            
-//                            // calculate fuel consumption difference in gallans
-//                            let dblDiffPercent = 100 - ((dblRealFuelConsumption * 100) / dblExpectedFuelConsumption);
-//                            document.getElementById('frmFuelEntry:diffPercent').innerHTML = String.valueOf(dblDiffPercent);
-//                            
-//                            // calculate fuel consumption difference in cash
-//                            let dblFuelDiffCash = dblDiffGallons * dblUnitCost;
-//                            document.getElementById('frmFuelEntry:diffCash').innerHTML = String.valueOf(dblFuelDiffCash);              
-//                        },
-//                        error: function() {
-//                            console.log("&lt;An error occured with API call&ht;");
-//                        }
-//                    });
-//                }
-//            </script>
+                    + "&from=" + strApiPrevFormattedTimestamp + "&to=" + 
+                    strApiCurrFormattedTimestamp;
+//                    System.out.println("strUrlWithQueries: " + strUrlWithQueries);
+            String usernameColonPassword = "nextop:nextop123";
+            String basicAuthPayload = "Basic " + Base64.getEncoder().encodeToString(usernameColonPassword.getBytes());
+
+            BufferedReader httpResponseReader = null;
+            // Connect to the web server endpoint
+            URL serverUrl = new URL(strUrlWithQueries);
+            HttpURLConnection urlConnection = (HttpURLConnection) serverUrl.openConnection();
+
+            // Set HTTP method as GET
+            urlConnection.setRequestMethod("GET");
+            
+            // set request accept type to JSON
+            urlConnection.addRequestProperty("Accept", "application/json");
+
+            // Include the HTTP Basic Authentication payload
+            urlConnection.addRequestProperty("Authorization", basicAuthPayload);
+            
+            urlConnection.addRequestProperty("User-Agent", "Mozilla/4.0");
+
+            // Read response from web server, which will trigger HTTP Basic Authentication request to be sent.
+            httpResponseReader =
+                    new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+            String lineRead;
+            String strJson = "";
+            while((lineRead = httpResponseReader.readLine()) != null) {
+//                System.out.println(lineRead);
+                strJson += lineRead;
+            }
+            System.out.println("strJson: " + strJson);
+            //calculate  and set total cost
+            current.setTotalCost(current.getGasRefillGals() * current.getUnitCost());
+            
+            ObjectMapper objMapper = new ObjectMapper();       
+            JsonNode objJsonNode = objMapper.readTree(strJson);
+            JsonNode objLastItem = objJsonNode.get(objJsonNode.size() - 1);
+            for(Object objItem : objLastItem) {
+                System.out.println(objItem);
+            }
+            // get attributes item and parse out io136 and io137
+            double dblFuelVal1 = objLastItem.get("attributes").get("io136") != null ?
+                    objLastItem.get("attributes").get("io136").asDouble() : -1.0;
+            double dblFuelVal2 = objLastItem.get("attributes").get("io137") != null 
+                    ? objLastItem.get("attributes").get("io137").asDouble() : -1.0;
+            
+            //calculate & set expected fuel consumption (currFillupKm - prevFillupKm) / 100)
+            current.setExpectedFuelConsumption(current.getLimeageKm() - dblPrevMileageKm / 100.0);
+                                     
+            // calculate and set real fuel consumption by either nozzle 
+            // crocodile or flowmeters
+            if(dblFuelVal1 > 0.0 && dblFuelVal2 > 0.0) {
+                // Flowmeters -> (io136/kfactor)-(io137/kfactor)
+                current.setRealConsumption((dblFuelVal1 / FLOWMETER1_DENOMINATOR) - (dblFuelVal2 / FLOWMETER2_DENOMINATOR));
+            } else { // only dblFuelCal1 has a value so use the nozzle denominator
+                current.setRealConsumption(dblFuelVal1 * NOZZLE_DENOMINATOR);
+            }
+            
+            // calculate and set difference in gallons
+            current.setDiffGals(current.getExpectedFuelConsumption() - current.getRealConsumption());
+
+            // calculate and set fuel consumption difference percent
+            current.setDiffPercent(100.0 - ((current.getRealConsumption() *  100.0) / current.getExpectedFuelConsumption()));
+                         
+            // calculate and set fuel consumption difference in cash
+            current.setDiffCash(current.getDiffGals() * current.getUnitCost());
             
             getFacade().create(current);
             JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("FuelConsumptionCreated"));
+
             return prepareCreate();
-        } catch (Exception e) {
+        } catch (IOException e) {
             JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
             return null;
+        } 
+    }
+    
+    private Object[] retrieveLastFuelEntryDateAndMileage() {
+        Object [] arrObj = null;
+        String strSql = "SELECT fuel_entry_dt, limeage_km FROM fuel_consumption ORDER BY fuel_entry_dt DESC FETCH FIRST 1 ROWS ONLY";
+        EntityManagerFactory objEntityMgrFactory = Persistence.createEntityManagerFactory("CostPerKmPU");
+        EntityManager objEntityMgr = objEntityMgrFactory.createEntityManager();
+        Query objQuery = objEntityMgr.createNativeQuery(strSql);
+        List <Object[]> lstFuelEntryDtAndMileage = objQuery.getResultList();
+        
+        if(lstFuelEntryDtAndMileage.size() > 0) {
+            arrObj = lstFuelEntryDtAndMileage.get(0);
+//            for(Object a : lstFuelEntryDtAndMileage) {
+//                System.out.println("Object a: " + a);
+//            }
         }
+//        TypedQuery<FuelConsumption> objTypedQuery = objEntityMgr.createQuery(strSql, FuelConsumption.class);      
+//        List lstFuelConsumption = objTypedQuery.getResultList();       
+//        System.out.println("lstFuelConsumption [");
+//        System.out.println("fuel_entry_dt: " + lstFuelConsumption.get(0));
+//        System.out.println("limeage_km: " + lstFuelConsumption.get(1));
+//        System.out.println("]");
+
+        return arrObj;
     }
 
     public String prepareEdit() {
